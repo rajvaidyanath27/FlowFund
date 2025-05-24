199419bd66559fa094269eba5ce927d932fbba70
Raj <rajbaidyanath76@gmail.com>
2025-05-24 23:45:37 +0530

    kkjnj

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Compare and choose the best credit or debit card tailored to your needs. Save time and money with our quick, step-by-step process that finds you the ideal card in minutes.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
