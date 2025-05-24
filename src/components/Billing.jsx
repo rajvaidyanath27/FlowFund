f1d7f16c9ae0ae5fb0525189a8c364a60e34a261
Raj <rajbaidyanath76@gmail.com>
2025-05-25 00:22:07 +0530

    done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -21,6 +21,9 @@ const Billing = () => (
        Manage all your billing and invoicing tasks in one place. Simplify payments, track transactions, and stay organized with ease.
       </p>
 
+     
+
+
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
         <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
         <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
